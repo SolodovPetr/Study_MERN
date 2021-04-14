6a596580ad510b4a217649cd7ab2e1ff8c960f34
Petr <solodov.petr@gmail.com>
2021-04-14 14:12:18 +0300

    User model - hash password and check email to be unique before save

--- server/models/user_model.js
+++ server/models/user_model.js
@@ -49,6 +49,23 @@ const userSchem = mongoose.Schema({
     // collection: 'members'  -- set custom name
 });
 
+// hash password before store to db
+userSchem.pre('save', async function (next) {
+    const user = this;
+    if ( user.isModified('password') ) {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(user.password, salt);
+        user.password = hash;
+    }
+    next();
+});
+
+// check for email to be unique in db
+userSchem.statics.emailExist = async function (email) {
+    const user = await this.findOne({email});
+    return !!user;
+}
+
 const User = mongoose.model('User', userSchem);
 
 module.exports = { User }
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -5,8 +5,27 @@ require('dotenv').config();
 const { User } = require('../../models/user_model');
 
 router.route('/register')
-    .post( (request, response ) => {
-       response.status(200).send('ok');
+    .post( async (request, response ) => {
+        try {
+            // check email to be unique
+            if ( await User.emailExist(request.body.email) ) {
+                return response.status(400).json({message: 'Email already exist.'});
+            }
+
+            // create model
+            const user = new User({
+                email: request.body.email,
+                password: request.body.password
+            });
+
+            // hash password on pre-save action
+            const doc = await user.save();
+
+            response.cookie('x-access-token', 'token-value')
+                .status(200).send(doc);
+        } catch (error) {
+            response.status(400).json({message: 'Auth error', error});
+        }
     });
 
 module.exports = router;
